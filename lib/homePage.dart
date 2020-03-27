d9a045534deb371100eee253e2a7fcd8a6c97321
Cedric <cedric@Cedrics-MacBook-Pro.local>
2020-03-27 16:36:56 +0100

    navigation part 2

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,46 +1,73 @@
 import 'package:flutter/material.dart';
-import 'firstScreen.dart';
+import 'package:project_1/firstScreen.dart';
 
-class HomePage extends StatelessWidget {
+class DrawerItem {
+  String title;
+  IconData icon;
+  DrawerItem(this.title, this.icon);
+}
+
+class HomePage extends StatefulWidget {
+  final drawerItems = [
+    new DrawerItem("Home screen", Icons.home),
+    new DrawerItem("First screen", Icons.games),
+  ];
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentDrawerItem = 0;
+
+  _onSelectItem(int index) {
+    setState(() => _currentDrawerItem = index);
+    Navigator.of(context).pop(); // close the drawer
+  }
+
+  _getDrawerItemWidget(int pos) {
+    switch (pos) {
+      case 0:
+        return new HomePage();
+      case 1:
+        return new FirstScreen();
+
+      default:
+        return new Text("Error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var drawerOptions = <Widget>[];
+    for (var i = 0; i < widget.drawerItems.length; i++) {
+      var drawerItem = widget.drawerItems[i];
+      drawerOptions.add(
+        new ListTile(
+          leading: new Icon(drawerItem.icon),
+          title: new Text(drawerItem.title),
+          selected: i == _currentDrawerItem,
+          onTap: () => _onSelectItem(i),
+        )
+      );
+    }
+
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: Text(widget.drawerItems[_currentDrawerItem].title),
       ),
       drawer: Drawer(
         child: Column(
           children: <Widget>[
             DrawerHeader(
-              child: Text('Header'),
+              child: Text('Header drawer'),
             ),
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => HomePage(),
-                  ),
-                );
-              },
-              child: Text('Home Screen'),
-            ),
-            FlatButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FirstScreen(),
-                  ),
-                );
-              },
-              child: Text('First Screen'),
-            ),
+            Column(children: drawerOptions)
           ],
         ),
       ),
+      body: _getDrawerItemWidget(_currentDrawerItem),
     );
   }
 }
