9e4c395aeab0f387b737e56e7d30d623e13bce7c
Cedric <cedric@Cedrics-MacBook-Pro.local>
2020-04-03 15:08:45 +0200

    add a bottom navigation bar per screen

--- lib/firstScreen.dart
+++ lib/firstScreen.dart
@@ -1,14 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:project_1/drawerComponent.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  int _firstScreenBNItem = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    // this are the different widgets to display as body
+    Text(
+      'Index 0: Games',
+    ),
+    Text(
+      'Index 1: Statistics',
+    ),
+    Text(
+      'Index 2: Settings',
+    ),
+  ];
+
+  _onItemTapped(int index) {
+    setState(() {
+      _firstScreenBNItem = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('First Screen'),
       ),
-      drawer: DrawerComponent(currentDrawerItem: 'First screen',)
+      drawer: DrawerComponent(currentDrawerItem: 'First screen'),
+      body: Center(
+        child: _widgetOptions.elementAt(_firstScreenBNItem),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.games),
+            title: Text('Games'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            title: Text('Statistics'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text('Settings'),
+          ),
+        ],
+        currentIndex: _firstScreenBNItem,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,14 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:project_1/drawerComponent.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _homePageBNItem = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[ // this are the different widgets to display as body
+    Text(
+      'Index 0: Home',
+    ),
+    Text(
+      'Index 1: Business',
+    ),
+    Text(
+      'Index 2: School',
+    ),
+  ];
+
+  _onItemTapped(int index) {
+    setState(() {
+      _homePageBNItem = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home Screen'),
       ),
-      drawer: DrawerComponent(currentDrawerItem: 'Home screen',)
+      drawer: DrawerComponent(currentDrawerItem: 'Home screen'),
+      body: Center(
+        child: _widgetOptions.elementAt(_homePageBNItem),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            title: Text('Business'),
+          ),
+        ],
+        currentIndex: _homePageBNItem,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
